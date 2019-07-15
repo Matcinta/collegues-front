a8b7e9b6ede132b4154c7b2acf828f4d26e95d77
Mathilde MUNIER <muniermathilde@gmail.com>
2019-07-15 17:11:05 +0200

    creation de composants avec angular

---
+++ src/app/mock/collegues.mock.ts
@@ -0,0 +1,3 @@
+import { Collegue } from '../models/Collegue';
+
+export let collegueMock = new Collegue ("cf9987ty89499gh794h64sg","Martin","Rose","rose.martin@gmail.com","1988-10-12","https://i.servimg.com/u/f62/09/02/30/17/1974_10.jpg") 
---
+++ src/app/models/Collegue.ts
@@ -0,0 +1,17 @@
+
+
+export class Collegue {
+
+
+
+constructor (
+    public matricule?:string,
+    public nom?: string,
+    public prenom?:string,
+    public email?: string,
+    public dateDeNaissance?:string,
+    public photoUrl?: string){
+
+}
+
+}
