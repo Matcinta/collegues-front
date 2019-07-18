ae2e7ced3e03cae5eda8e86d97b1da0a5f98c312
Mathilde MUNIER <muniermathilde@gmail.com>
2019-07-18 15:52:11 +0200

    tp6 optimisation ergonomie navigation entre les composants

--- src/app/form-creer-collegue/form-creer-collegue.component.ts
+++ src/app/form-creer-collegue/form-creer-collegue.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { Collegue } from '../models/Collegue';
 import { DataService } from '../services/data.service';
 
@@ -9,14 +9,20 @@ import { DataService } from '../services/data.service';
 })
 export class FormCreerCollegueComponent implements OnInit {
 
-  @Input()
-  col: Collegue = new Collegue();
-
-
   constructor(private srv: DataService) { }
 
   ngOnInit() {
   }
+
+  @Input()
+  col: Collegue = new Collegue();
+
+  @Output() annuler = new EventEmitter<boolean>();
+
+  clickAnnuler(){
+    this.annuler.emit(true);
+  }
+
 
   submit(){
   this.srv.ajouterCollegue(this.col.nom, this.col.prenom, this.col.email, this.col.dateDeNaissance, this.col.photoUrl)
