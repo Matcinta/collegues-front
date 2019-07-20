cbd84ca0bab63794a38bfc7405e24e7a4e885d91
Mathilde Mnr <muniermathilde@gmail.com>
2019-07-20 18:20:39 +0200

    ajout saisie de notes et affichage liste notes en local

--- src/app/detail-collegue/detail-collegue.component.ts
+++ src/app/detail-collegue/detail-collegue.component.ts
@@ -2,16 +2,21 @@ import { Component, OnInit, Input } from '@angular/core';
 import { DataService } from '../services/data.service';
 import { ActivatedRoute, ParamMap } from '@angular/router';
 import { Collegue } from '../models/Collegue';
+import {Comment} from '../models/Comment';
 
 @Component({
   selector: 'app-detail-collegue',
   templateUrl: './detail-collegue.component.html',
   styleUrls: ['./detail-collegue.component.css']
 })
+
 export class DetailCollegueComponent implements OnInit {
 
-  col: Collegue;
+  col: Collegue = new Collegue();
   matricule: string;
+  comments=[];
+  comment: Comment = new Comment();
+  newComment = false;
 
   constructor(private srv: DataService, private route: ActivatedRoute) {
 
@@ -26,6 +31,16 @@ export class DetailCollegueComponent implements OnInit {
     
   }
 
+  addComment(){
+    if(this.comment.message != ''){
+      this.comment.id= this.comments.length+1;
+      this.comments.push({
+        id:this.comment.id,
+        message:this.comment.message}
+        );
+        this.comment.message='';
+    }
+  }
   
     
 
---
+++ src/app/models/Comment.ts
@@ -0,0 +1,12 @@
+export class Comment {
+
+
+
+    constructor (
+        public id?:number,
+        public message?: string,
+        public date?:Date){
+    
+    }
+    
+    }
