89f3d4a2e6c3391ff520b7f3501b27fbe2ac6061
Mathilde MUNIER <muniermathilde@gmail.com>
2019-07-23 17:45:13 +0200

    suppression des erreurs de navigation hors connexion

--- src/app/services/connexion.guard.ts
+++ src/app/services/connexion.guard.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, RouterStateSnapshot, CanActivate, Router } from '@angular/router';
 import { AuthService } from './auth.service';
+import { map, catchError } from 'rxjs/operators';
+import { of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -10,10 +12,11 @@ export class ConnexionGuard implements CanActivate  {
   constructor(private router: Router, private authService: AuthService) { }
   
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
-    if(!this.authService.isConnected()){
-      this.router.navigate(['/login']);
-    }
-    return this.authService.isConnected();
+    
+    return this.authService.isConnected()
+    .pipe(
+     catchError(() => of( this.router.parseUrl('/loggin')))
+    );
   }
 }
   
